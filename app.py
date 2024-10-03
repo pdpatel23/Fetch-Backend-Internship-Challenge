ace678e834f2470a8649633ed0a6b0b4dc867ed4
Puja Patel <pujadpatel23@gmail.com>
2024-10-03 00:02:00 -0500

    implemented the spend endpoint

--- app.py
+++ app.py
@@ -36,6 +36,57 @@ def add_points():
    
     return "", 200
 
+#spends points starting form oldest transaction without having payer's balance go negative
+@app.route('/spend', methods=['POST'])
+def spend_points():
+
+    #extract points
+    data = request.get_json()
+    points_to_spend = data['points']
+
+    #Check if there are enough points
+    total_points = sum(payer_balances.values())
+    if points_to_spend > total_points:
+        return "The user doesn't have enough points", 400
+
+    #sort transactions by time
+    spent_points = []
+    sorted_transactions = sorted(transactions, key=lambda x: x['timestamp'])
+ 
+    #deduct points from payer starting with oldest transaction
+    for transaction in sorted_transactions:
+        if points_to_spend <= 0:
+            break
+
+        payer = transaction['payer']
+        points = transaction['points']
+
+        points_deducted = min(points, points_to_spend)
+        transaction['points'] -= points_deducted
+        payer_balances[payer] -= points_deducted
+
+
+        spent_points.append({'payer': payer, 'points': -points_deducted})
+
+        points_to_spend -= points_deducted
+        
+    output_list = {}
+
+    for transaction in spent_points:
+        payer = transaction['payer']
+        points = transaction['points']
+
+        if payer in output_list:
+            output_list[payer] += points
+        else:
+            output_list[payer] = points
+   
+    output_list = [{'payer': payer, 'points': points} for payer, points in output_list.items()]
+
+
+    return jsonify(output_list), 200
+
+
 #return current point balance for each payer
 @app.route('/balance', methods=['GET'])
 def get_balance():
